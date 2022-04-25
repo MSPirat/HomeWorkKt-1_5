c53b84dd326c58010021d8c20824d44bc9502a9e
Sergey <93938031+MSPirat@users.noreply.github.com>
2022-04-25 17:16:49 +0600

    Modified Post.kt (added more fields) Created WallService.kt (fun add, update, likeById)

--- src/main/kotlin/ru/netology/Post.kt
+++ src/main/kotlin/ru/netology/Post.kt
@@ -4,28 +4,17 @@ import java.util.Objects
 
 data class Post(
 	val postId: Long,
+	val ownerId: Long,
 	val authorId: Long,
 	val authorName: String,
 	val dateTime: Long,    //https://i-leon.ru/tools/time
 	val text: String,
+	val replyOwnerId: Long,
+	val replyPostId: Long,
+	val friendsOnly: Boolean,
 	val comments: Objects,
-	val likes: Int = 0,
+	val likes: Int, //= 0,
+	val reposts: Objects,
 	val views: Objects,
+	val isFavorite: Boolean,
 )
-
-object WallService {
-	private var posts = emptyArray<Post>()
-
-	fun add(post: Post): Post {
-		posts += post
-		return posts.last()
-	}
-
-	fun likeById(id: Long) {
-		for ((index, post) in posts.withIndex()) {
-			if (post.postId == id) {
-				posts[index] = post.copy(likes = post.likes + 1)
-			}
-		}
-	}
-}
---
+++ src/main/kotlin/ru/netology/WallService.kt
@@ -0,0 +1,34 @@
+package ru.netology
+
+object WallService {
+	private var posts = emptyArray<Post>()
+	private var nextId: Long = 0
+
+	fun add(post: Post): Post {
+		posts += if (posts.isEmpty()) {
+			post.copy(postId = 0)
+		} else {
+			post.copy(postId = nextId)
+		}
+		nextId++
+		return posts.last()
+	}
+
+	fun update(post: Post): Boolean {
+		for ((postId, newPost) in posts.withIndex()) {
+			if (newPost.postId == post.postId) {
+				posts[postId] = newPost.copy(ownerId = newPost.ownerId, dateTime = newPost.dateTime)
+				return true
+			}
+		}
+		return false
+	}
+
+	fun likeById(id: Long) {
+		for ((index, post) in posts.withIndex()) {
+			if (post.postId == id) {
+				posts[index] = post.copy(likes = post.likes + 1)
+			}
+		}
+	}
+}
