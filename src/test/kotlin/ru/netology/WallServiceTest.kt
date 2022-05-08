6e4e22ecb84fdc35a17492d40e4f62165093f1cc
Sergey Melnichenko <m1s3p@mail.ru>
2022-05-08 22:38:55 +0600

    Change WallServiceTest.kt with postId Modified WallService.kt in update function

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -74,7 +74,24 @@ class WallServiceTest {
 		assertFalse(result)
 	}
 
-	@Test
-	fun likeById() {
-	}
+    /*
+    @Test
+    fun updateExisting() {
+        // создаём целевой сервис
+        val service = WallService()
+        // заполняем несколькими постами
+        service.add(Post(/* заполняете поля */))
+        service.add(Post(/* заполняете поля */))
+        service.add(Post(/* заполняете поля */))
+        // создаём информацию об обновлении
+        val update = Post(/* заполняете поля */)
+
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertTrue(result)
+    }
+
+     */
 }
